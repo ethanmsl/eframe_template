0b3c9fffc6e05803f85be05c6cc8dbb5b636f56a
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-05-28 04:25:48 -0400

    Feat: cargo-generate comp mustache templating

--- assets/sw.js
+++ assets/sw.js
@@ -2,8 +2,8 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './{{crate_name | snake_case}}.js',
+  './{{crate_name | snake_case}}_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
